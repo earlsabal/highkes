1971dfbe8b1670567f5f9284db4265e66a318862
Earl Sabal <earlsabal@gmail.com>
2017-01-25 17:53:37 -0800

    Initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
         bucket: ENV.fetch('S3_BUCKET_NAME')
         aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
         secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-      },
+      }
   }
   config.cache_classes = true
   config.serve_static_assets = true
