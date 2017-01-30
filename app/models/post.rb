a76d43c63cc24527c5f428f9786c1e60f0c0e463
Earl Sabal <earlsabal@gmail.com>
2017-01-30 15:46:27 -0800

    Intial commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	validates :image, presence: true
 
-	has_attached_file :image, :default_url => "/images/emptyuser.png"
+	has_attached_file :image, :url =>':s3_domain_url'
 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
