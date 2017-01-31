42fd9c7cb8e7d6286735ae4177890e6751984270
Earl Sabal <earlsabal@gmail.com>
2017-01-30 16:39:22 -0800

    Intial commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	validates :image, presence: true
 
-	has_attached_file :image
+	has_attached_file :image, '/posts/image/:id/'
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
