39eb5e6b8c769f495f8c6ba16c68f9f8b671b1b0
Earl Sabal <earlsabal@gmail.com>
2017-01-31 11:48:09 -0800

    Intial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    s3_host_name: 's3-us-west-2.amazonaws.com',
+    s3_host_name: 's3-us-west-1.amazonaws.com',
     s3_credentials: {
       bucket: ENV['AWS_S3_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
