08d3fa301cc4b19cf86ed3d5be054bd18e56d47b
Earl Sabal <earlsabal@gmail.com>
2017-02-01 12:27:38 -0800

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125200022) do
+ActiveRecord::Schema.define(version: 20170201185249) do
 
   create_table "posts", force: true do |t|
     t.string   "caption"
@@ -37,9 +37,11 @@ ActiveRecord::Schema.define(version: 20170125200022) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
