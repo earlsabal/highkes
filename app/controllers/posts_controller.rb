f1dc088283526812259e61ac24151a4abe41aa83
Earl Sabal <earlsabal@gmail.com>
2017-03-29 18:24:31 -0700

    App now shows newest posts first

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 	before_action :authenticate_user!
 	before_action :set_post, only: [:show, :edit, :update, :destroy, :likes]
 	def index
-		@posts = Post.paginate(page: params[:page], per_page: 5)
+		@posts = Post.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
 	end
 
 	def show
