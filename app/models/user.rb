de75a0ea13d8175a48d8338dec37dd7d739c7204
Earl Sabal <earlsabal@gmail.com>
2017-03-29 23:06:05 -0700

    Added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ActiveRecord::Base
 	has_many :comments
 	has_attached_file :image, :source_file_options => { :all => '-auto-orient' }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates :email,
+	  presence: true,
+	  uniqueness: {case_sensitive: false}
+	validates :username,
+ 	  :presence => true,
+ 	  :uniqueness => {:case_sensitive => false} 
+   	validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
 
 	def like!(post)
 	  self.likes.create!(post_id: post.id)
